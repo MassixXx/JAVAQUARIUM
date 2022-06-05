b5834de1570b4ff4c94153e5e477b977037bbb3d
MassixXx <km_abboud@esi.dz>
2022-06-05 23:03:44 +0100

    ajout du projet complet

---
+++ CMakeFiles/JAVAQUARIUM.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hp/Documents/Code/C++/JAVAQUARIUM/algue.cpp" "/home/hp/Documents/Code/C++/JAVAQUARIUM/cmake-build-debug/CMakeFiles/JAVAQUARIUM.dir/algue.cpp.o"
+  "/home/hp/Documents/Code/C++/JAVAQUARIUM/aquarium.cpp" "/home/hp/Documents/Code/C++/JAVAQUARIUM/cmake-build-debug/CMakeFiles/JAVAQUARIUM.dir/aquarium.cpp.o"
+  "/home/hp/Documents/Code/C++/JAVAQUARIUM/general.cpp" "/home/hp/Documents/Code/C++/JAVAQUARIUM/cmake-build-debug/CMakeFiles/JAVAQUARIUM.dir/general.cpp.o"
+  "/home/hp/Documents/Code/C++/JAVAQUARIUM/main.cpp" "/home/hp/Documents/Code/C++/JAVAQUARIUM/cmake-build-debug/CMakeFiles/JAVAQUARIUM.dir/main.cpp.o"
+  "/home/hp/Documents/Code/C++/JAVAQUARIUM/objetaquarium.cpp" "/home/hp/Documents/Code/C++/JAVAQUARIUM/cmake-build-debug/CMakeFiles/JAVAQUARIUM.dir/objetaquarium.cpp.o"
+  "/home/hp/Documents/Code/C++/JAVAQUARIUM/poisson.cpp" "/home/hp/Documents/Code/C++/JAVAQUARIUM/cmake-build-debug/CMakeFiles/JAVAQUARIUM.dir/poisson.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/JAVAQUARIUM.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JAVAQUARIUM.dir/algue.cpp.o"
+  "CMakeFiles/JAVAQUARIUM.dir/aquarium.cpp.o"
+  "CMakeFiles/JAVAQUARIUM.dir/general.cpp.o"
+  "CMakeFiles/JAVAQUARIUM.dir/main.cpp.o"
+  "CMakeFiles/JAVAQUARIUM.dir/objetaquarium.cpp.o"
+  "CMakeFiles/JAVAQUARIUM.dir/poisson.cpp.o"
+  "JAVAQUARIUM"
+  "JAVAQUARIUM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JAVAQUARIUM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
